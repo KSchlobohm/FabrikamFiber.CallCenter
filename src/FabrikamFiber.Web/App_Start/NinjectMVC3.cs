a121fbd98edc60e8cade28a401ac0c0e47d29ad5
Ken Schlobohm <keschlob@microsoft.com>
2022-06-21 15:24:43 -0500

    migration to OWIN with Ninject support

--- src/FabrikamFiber.Web/App_Start/NinjectMVC3.cs
+++ src/FabrikamFiber.Web/App_Start/NinjectMVC3.cs
@@ -1,4 +1,4 @@
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(FabrikamFiber.Web.App_Start.NinjectMVC3), "Start")]
+//[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(FabrikamFiber.Web.App_Start.NinjectMVC3), "Start")]
 //[assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(FabrikamFiber.Web.App_Start.NinjectMVC3), "Stop")]
 
 namespace FabrikamFiber.Web.App_Start
@@ -10,30 +10,30 @@ namespace FabrikamFiber.Web.App_Start
 
     public static class NinjectMVC3 
     {
-        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+        //private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
-        /// <summary>
-        /// Starts the application
-        /// </summary>
-        public static void Start() 
-        {
-            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
-            bootstrapper.Initialize(CreateKernel);
-        }
+        ///// <summary>
+        ///// Starts the application
+        ///// </summary>
+        //public static void Start() 
+        //{
+        //    DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+        //    bootstrapper.Initialize(CreateKernel);
+        //}
 
-        /// <summary>
-        /// Stops the application.
-        /// </summary>
-        public static void Stop()
-        {
-            bootstrapper.ShutDown();
-        }
+        ///// <summary>
+        ///// Stops the application.
+        ///// </summary>
+        //public static void Stop()
+        //{
+        //    bootstrapper.ShutDown();
+        //}
 
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
         /// <returns>The created kernel.</returns>
-        private static IKernel CreateKernel()
+        internal static IKernel CreateKernel()
         {
             var kernel = new StandardKernel();
             RegisterServices(kernel);
---
+++ src/FabrikamFiber.Web/Startup.cs
@@ -0,0 +1,17 @@
+﻿using FabrikamFiber.Web.App_Start;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FabrikamFiber.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNinject(NinjectMVC3.CreateKernel);
+        }
+    }
+}
